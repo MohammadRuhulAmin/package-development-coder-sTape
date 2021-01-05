0057c1bcbee0b9c1e40871e8a8d64c17bcbcb2e2
Mohammad Ruhul Amin <rA.01403288711@gmail.com>
2021-01-05 18:10:36 +0600

    just testing onetime

---
+++ tests/Feature/InitialTest.php
@@ -0,0 +1,12 @@
+<?php 
+
+namespace vicgonvt\Press\Tests;
+
+use Orchestra\Testbench\TestCase;
+
+class InitialTest extends TestCase{
+
+    public function my_first_test(){
+        $this->assertTrue(true);
+    }
+}
